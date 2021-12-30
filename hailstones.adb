f4f807e1f4e93bc888b4c1f2908e9e12c0020938
Carlin Bingham <cb@viennan.net>
2021-12-30 12:10:42 +0000

    multithreaded

--- hailstones.adb
+++ hailstones.adb
@@ -4,6 +4,77 @@ procedure Hailstones
 is
    type Number is mod 2 ** 64;
 
+   Total_Tasks : constant Number := 6;
+   Upper_Limit : constant Number := 100_000_000;
+
+   function Count_Steps (Initial : Number) return Number;
+
+   protected Calculated_Object is
+      procedure Propose (Steps : Number; N : Number);
+      procedure Finished;
+
+      entry Get_Steps (V : out Number);
+      entry Get_Best  (V : out Number);
+   private
+      Count : Number := 0;
+      Best  : Number := 0;
+      Busy  : Number := Total_Tasks;
+      Ready : Boolean := False;
+   end Calculated_Object;
+
+   protected body Calculated_Object is
+      procedure Propose (Steps : Number; N : Number) is
+      begin
+         if Steps > Count then
+            Count := Steps;
+            Best  := N;
+         end if;
+      end Propose;
+
+      procedure Finished is
+      begin
+         Busy := Busy - 1;
+         if Busy = 0 then
+            Ready := True;
+         end if;
+      end Finished;
+
+      entry Get_Steps (V : out Number)
+         when Ready
+      is
+      begin
+         V := Count;
+      end Get_Steps;
+
+      entry Get_Best (V : out Number)
+         when Ready
+      is
+      begin
+         V := Best;
+      end Get_Best;
+   end Calculated_Object;
+
+   task type Calculate_Task is
+      entry Start (Initial : Number);
+   end Calculate_Task;
+
+   task body Calculate_Task is
+      N : Number;
+      Current : Number := 0;
+   begin
+      accept Start (Initial : Number) do
+         N := Initial;
+      end Start;
+      loop
+         exit when N >= Upper_Limit;
+         Current := Count_Steps (N);
+         Calculated_Object.Propose (Current, N);
+
+         N := N + Total_Tasks;
+      end loop;
+      Calculated_Object.Finished;
+   end Calculate_Task;
+
    function Count_Steps (Initial : Number) return Number
    is
       Steps : Number := 0;
@@ -21,18 +92,17 @@ is
       return Steps;
    end Count_Steps;
 
-   Longest : Number := 0;
-   Current : Number := 0;
-   Best    : Number;
+   Tasks : array (1.. Total_Tasks) of Calculate_Task;
+   Longest_Steps : Number;
+   Best_Instigator : Number;
 begin
-   for I in 1 .. 100000 loop
-      Current := Count_Steps (Number (I));
-      if Current > Longest then
-         Longest := Current;
-         Best := Number (I);
-      end if;
+   for I in Tasks'range loop
+      Tasks (I).Start (I);
+      Ada.Text_IO.Put_Line ("Started task");
    end loop;
 
-   Ada.Text_IO.Put_Line (Number'Image (Best));
+   Calculated_Object.Get_Steps (Longest_Steps);
+   Calculated_Object.Get_Best  (Best_Instigator);
+   Ada.Text_IO.Put_Line (Number'Image (Longest_Steps));
+   Ada.Text_IO.Put_Line (Number'Image (Best_Instigator));
 end Hailstones;
-
