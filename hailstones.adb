e038bc66e62d552d1d80c86bb5f8159117ebc914
Carlin Bingham <cb@viennan.net>
2021-12-30 09:18:00 +0000

    longest sequence

--- hailstones.adb
+++ hailstones.adb
@@ -2,16 +2,30 @@ with Ada.Text_IO;
 
 procedure Hailstones
 is
-   N : Natural := 1000000;
+
+   type Number is mod 2 ** 64;
+
+   Longest : Number := 0;
+   Current : Number := 0;
 begin
-   loop
-      Ada.Text_IO.Put_Line (Natural'Image (N));
-      exit when N = 1;
-      if N mod 2 = 0 then
-         N := N / 2;
-      else
-         N := (3 * N) + 1;
-      end if;
+   for I in 1 .. 1000000 loop
+      declare
+         N : Number := Number (I);
+      begin
+         while N /= 1 loop
+            if N mod 2 = 0 then
+               N := N / 2;
+            else
+               N := (3 * N) + 1;
+            end if;
+            Current := Current + 1;
+         end loop;
+         if Current > Longest then
+            Longest := Current;
+         end if;
+      end;
    end loop;
+
+   Ada.Text_IO.Put_Line (Number'Image (Longest));
 end Hailstones;
 
